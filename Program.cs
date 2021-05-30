e538d30847d3f6618a2b0f3a9fce5a535b80b356
Sahanaumesh7 <38214407+Sahanaumesh7@users.noreply.github.com>
2021-05-30 18:38:00 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sampletest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
